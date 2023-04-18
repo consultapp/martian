25afde75cb5b9ec1cab797cefa5886e11e61e178
Dmitry Egorov <info@consultapp.ru>
2023-04-18 14:42:00 +0400

    pdf

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -4,7 +4,7 @@ DownloadButton
 
 export const DownloadButton = ({ resultName, side }) => (
   <a
-    href={"/static/pdf/" + resultName}
+    href={"/pdf/" + resultName}
     className="btn btn-primary m-2"
     download={`vizitka_${side}.pdf`}
     key={resultName}
